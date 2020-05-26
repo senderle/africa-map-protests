f78bb06a33d86a96d906f1cb33be25318446734b
Scott Enderle <scott.enderle@gmail.com>
2020-05-25 20:21:13 -0400

    use geopandas for protests and better country shapefiles

--- map.py
+++ map.py
@@ -2,9 +2,6 @@ import math
 
 from collections import defaultdict, Counter
 
-import pycountry
-from reverse_geocoder import search as reverse_geo
-
 # from shapely.geometry import Point, Polygon
 import pandas
 import geopandas as gpd
@@ -21,8 +18,6 @@ from bokeh.tile_providers import (
     CARTODBPOSITRON_RETINA,
     get_provider
 )
-
-# This is the function for making the map of africa graph
 
 
 def lat_lon_to_web_mercator(lon, lat):
@@ -66,10 +61,14 @@ def multipolygons_to_xs_ys(multipolygons):
 # The closest thing is Bokeh's MultiPolygons glyph, but it
 # doesn't accept shapely objects or geojson or anything
 # like that. Wah wah. So instead we have to do this by hand.
-def geodf_to_cds(geodf):
+# Also, Infinity isn't a valid JSON value, even though pandas
+# seems to think it is.
+def geodf_patches_to_geods(geodf):
     geo_xs, geo_ys = multipolygons_to_xs_ys(geodf['geometry'])
     geodf = geodf.assign(xs=geo_xs, ys=geo_ys)
-    return GeoJSONDataSource(geojson=geodf.to_json())
+    return GeoJSONDataSource(
+        geojson=geodf.to_json().replace('Infinity', 'null')
+    )
 
 
 def safe_lt(comp):
@@ -81,15 +80,6 @@ def safe_lt(comp):
     return comp_func
 
 
-def load_shapefile():
-    gdf = gpd.read_file('Africa/Africa.shp')
-    gdf.crs = {'init': 'epsg:4326'}
-    gdf = gdf.to_crs('EPSG:3857')
-    gdf['admin'] = gdf['COUNTRY']
-    print(gdf.head())
-    return gdf
-
-
 def can_be_simplified(val, tol=10.0):
     try:
         val.simplify(tol)
@@ -98,48 +88,50 @@ def can_be_simplified(val, tol=10.0):
         return False
 
 
-def load_geojson(simple=True, simplify_tol=None):
-    if simple:
-        gdf = gpd.read_file('Africa/africa-nations.json')
-        gdf = gdf.to_crs('EPSG:3857')
-        return gdf
-    else:
-        # Use simple shape data for Africa to thin out global shape data.
-        africa = gpd.read_file('Africa/africa-nations.json')[['admin']]
+def load_geojson(simplify_tol=None):
+    gdf = gpd.read_file('Africa/gadm28_countries.geojson')
+    gdf = gdf[gdf['geometry'].notna()]
 
-        gdf = gpd.read_file('Africa/countries.geojson')
-        gdf['admin'] = gdf['ADMIN']
-        gdf = gdf.set_index('ADMIN')
-        gdf = gdf.loc[africa['admin']]
+    # For all countries, drop just Antarctica:
+    # gdf = gdf[gdf['name_engli'] != 'Antarctica']
 
-        # Project from lat,lon data to web mercator.
-        gdf = gdf.to_crs('EPSG:3857')
+    # For just africa drop other continents:
+    gdf = gdf[gdf['unregion2'] == 'Africa']
 
-        # Use shapely simplification routine if simplify_tol is specified.
-        if simplify_tol is not None:
-            gdf = gdf[gdf['geometry'].apply(can_be_simplified)]
-            gdf['geometry'] = gdf['geometry'].simplify(
-                simplify_tol,
-                preserve_topology=False
-            )
-        return gdf
+    gdf['name'] = gdf['name_engli']
+    gdf = gdf.set_index('name_engli')
+
+    # Project from lat, lon data to web mercator.
+    gdf = gdf.to_crs('EPSG:3857')
+
+    # Use shapely simplification routine if simplify_tol is specified.
+    if simplify_tol is not None:
+        gdf = gdf[gdf['geometry'].apply(can_be_simplified)]
+        gdf['geometry'] = gdf['geometry'].simplify(
+            simplify_tol,
+            preserve_topology=False
+        )
+    return gdf
 
 
 def load_protests():
     protests = pandas.read_csv('protests.csv')
-    protests_wrong_long = protests[
-        (protests.LONG == 'checked') | (protests.LONG.apply(safe_lt(-20)))
+    protests_wrong_data = protests[
+        (protests.LONG == 'checked') | (protests.LONG.apply(safe_lt(-20))) |
+        (protests.LONG.isna()) | (protests.LAT.isna())
     ]
-    protests = protests.drop(protests_wrong_long.index, axis='rows')
-    lats = list(map(float, protests.LAT))
-    lons = list(map(float, protests.LONG))
-    x, y = zip(*map(lat_lon_to_web_mercator, lons, lats))
-    return dict(
-        x=x,
-        y=y,
-        lons=lons,
-        lats=lats,
+    protests = protests.drop(protests_wrong_data.index, axis='rows')
+    protests['LAT'] = protests.LAT.apply(float)
+    protests['LONG'] = protests.LONG.apply(float)
+    protests = protests[~((protests.LAT == 0) & (protests.LONG == 0))]
+
+    protests = gpd.GeoDataFrame(
+        protests,
+        geometry=gpd.points_from_xy(protests.LONG, protests.LAT),
+        crs='epsg:4326'  # CRS code for basic lat/lon data.
     )
+    protests = protests.to_crs('EPSG:3857')  # CRS code for web mercator.
+    return protests
 
 
 def load_protest_reverse():
@@ -156,58 +148,36 @@ def save_protest_reverse(data):
     df.to_csv('protest-reverse-cache.csv')
 
 
-_special_names_shapefile = {
-    "Côte d'Ivoire": 'Cote d`Ivoire',
-    'Congo, The Democratic Republic of the': 'Democratic Republic of Congo',
-    'Congo': 'Congo-Brazzaville'
-}
-_special_names_geojson = {
-    'Congo': 'Republic of Congo',
-    'Congo, The Democratic Republic of the':
-        'Democratic Republic of the Congo',
-    "Côte d'Ivoire": 'Ivory Coast'
+_name_errors = {
+    'Madagascar ': 'Madagascar',
+    "Cote d'lvoire": "Côte d'Ivoire",
+    'Djbouti': 'Djibouti',
+    'Malawi ': 'Malawi',
+    'Mauritus': 'Mauritius',
+    'Mauritania ': 'Mauritania',
+    'Congo- Brazzaville': 'Republic of Congo',
+    'Congo - Kinshasa': 'Democratic Republic of the Congo',
+    'Guinea Bissau': 'Guinea-Bissau'
 }
 
 
 def sum_protests(protests, nations):
     counts = defaultdict(int)
-    lons = protests['lons']
-    lats = protests['lats']
 
-    geo_data = load_protest_reverse()
-    if geo_data is None:
-        geo_data = []
-        for i, (lat, lon) in enumerate(zip(lats, lons)):
-            if not i % 10:
-                print(f'{i}/{len(lats)}')
-            try:
-                rg = reverse_geo((lat, lon))[0]
-            except IndexError:
-                continue
+    names = [_name_errors[n] if n in _name_errors else n
+             for n in protests.Name]
+    counts = Counter(names)
 
-            cname = pycountry.countries.get(alpha_2=rg['cc']).name
-            if cname in _special_names_geojson:
-                cname = _special_names_geojson[cname]
-            # if cname in _special_names_shapefile:
-            #     cname = _special_names_shapefile[cname]
-            counts[cname] += 1
-            rg['countryname'] = cname
-            geo_data.append(rg)
+    print(set(counts) - set(nations['name']))
+    print(set(nations['name']) - set(counts))
 
-        save_protest_reverse(geo_data)
-    else:
-        counts = Counter(geo_data['countryname'])
-
-    print(set(counts) - set(nations['admin']))
-    print(set(nations['admin']) - set(counts))
-
-    nations['protestcount'] = [counts[n] for n in nations['admin']]
+    nations['protestcount'] = [counts[n] for n in nations['name']]
 
     nation_rank = sorted(set(counts.values()), reverse=True)
     nation_rank.append(0)
     nation_rank = {c: i for i, c in enumerate(nation_rank)}
-    nation_rank = {n: nation_rank[counts[n]] for n in nations['admin']}
-    nations['rank'] = [nation_rank[n] for n in nations['admin']]
+    nation_rank = {n: nation_rank[counts[n]] for n in nations['name']}
+    nations['rank'] = [nation_rank[n] for n in nations['name']]
 
 
 def base_map():
@@ -221,11 +191,14 @@ def base_map():
         x_range=(-2300000, 6300000), y_range=(-4300000, 4600000),
         x_axis_type="mercator", y_axis_type="mercator",
         )
+    p.toolbar_location = None
 
     # tile_provider = get_provider(STAMEN_TONER)
     # tile_provider.url = ('http://tile.stamen.com/toner-lite/'
     #                      '{Z}/{X}/{Y}@2x.png')
     tile_provider = get_provider(CARTODBPOSITRON_RETINA)
+    # tile_provider.url = ('https://tiles.basemaps.cartocdn.com/'
+    #                      'light_only_labels/{z}/{x}/{y}@2x.png')
     p.add_tile(tile_provider)
     p.grid.grid_line_color = None
 
@@ -246,7 +219,7 @@ def patches(plot, patch_data):
         fill_alpha=0.5, line_color="purple", line_alpha=0.8,
         line_width=3.0
     )
-    render = plot.add_glyph(geodf_to_cds(patch_data),
+    render = plot.add_glyph(geodf_patches_to_geods(patch_data),
                             patches,
                             hover_glyph=hover_patches,
                             selection_glyph=patches,
@@ -271,8 +244,7 @@ def patches(plot, patch_data):
 def points(plot, point_data):
     point = Circle(x='x', y='y', fill_color="purple", fill_alpha=0.5,
                    line_color="gray", line_alpha=0.5, size=6)
-    plot.add_glyph(ColumnDataSource(point_data),
-                   point)
+    plot.add_glyph(GeoJSONDataSource(geojson=point_data.to_json()), point)
 
 
 if __name__ == "__main__":
